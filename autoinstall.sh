d259049d171e2d4c8cabb7abeefe2c217ccf662b
David Bates <bates@mbp-988g.ifeelfine.ca>
2018-03-25 04:01:46 -0400

    Revert "remove directory at end"
    This reverts commit 74c2ca7c9f900361be6ae5c385b87be050feae3b.

--- autoinstall.sh
+++ autoinstall.sh
@@ -33,4 +33,3 @@ echo -e "${LB}        "$substep")${NC} Ready to install" ; substep="$(echo $subs
 echo -e ""
 read -t 5 -p "Do you wish to begin the installation? [Y/n]" begin
 [[ -z $begin || ${begin,,} == "y" ]] || exit && sh /tmp/MediaServer/media_server.sh
-rm -rf /tmp/MediaServer/
